cd6ef9ee7f1cc71eb7f39a62241abb78ef3aa581
raikouo <gogotea4949@gmail.com>
2020-08-12 19:22:11 +0900

    password以外のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password, format: { with: /\A[a-z\d]{6,}+\z/i, message: '半角英数字6文字以上で入力してください' }
+    validates :birth_day
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
 end
