c6fdd50cb065a5fd3466c7169a606929ce992dc1
raikouo <gogotea4949@gmail.com>
2020-08-13 11:34:18 +0900

    gimei導入、test用データ準備

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,15 +3,16 @@
 
 FactoryBot.define do
   factory :user do
+    gimei = Gimei.name
     nickname {Faker::Games::Pokemon.name}
     email {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
-    first_name {Faker::Name.last_name}
-    last_name {Faker::Name.first_name}
-    first_name_kana {"サトウ"}
-    last_name_kana {"イチロウ"}
+    first_name {gimei.last.kanji}
+    last_name {gimei.first.kanji}
+    first_name_kana {gimei.last.katakana}
+    last_name_kana {gimei.first.katakana}
     birth_day {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
   end
 end
