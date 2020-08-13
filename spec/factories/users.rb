2dd6e524eae5c52651db92c779a35c99d50a09b1
raikouo <gogotea4949@gmail.com>
2020-08-13 15:41:55 +0900

    テストデータ修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,14 @@
 FactoryBot.define do
   factory :user do
-    gimei = Gimei.new
     nickname {Faker::Games::Pokemon.name}
     email {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
-    first_name {gimei.last.kanji}
-    last_name {gimei.first.kanji}
-    first_name_kana {gimei.last.katakana}
-    last_name_kana {gimei.first.katakana}
+    first_name {Gimei.last.kanji}
+    last_name {Gimei.first.kanji}
+    first_name_kana {Gimei.last.katakana}
+    last_name_kana {Gimei.first.katakana}
     birth_day {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
   end
 end
