cfd37383ac64758b50e5f143ec154b3375982812
raikouo <gogotea4949@gmail.com>
2020-08-28 04:11:42 +0900

    パンくず実装（リンク済）

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -3,31 +3,31 @@ crumb :root do
 end
 
 crumb :sign_up do
-  link "ユーザー登録"
+  link "ユーザー登録", new_user_registration_path
   parent :root
 end
 
 crumb :sign_in do
-  link "ログイン"
+  link "ログイン", new_user_session_path
   parent :root
 end
 
 crumb :item_new do
-  link "商品出品"
+  link "商品出品", new_item_path
   parent :root
 end
 
-crumb :item_edit do
-  link "商品編集"
-  parent :item_show
-end
-
-crumb :item_show do
-  link "商品詳細"
+crumb :item_show do |item|
+  link "#{item.id}: #{item.name}", item_path(item)
   parent :root
 end
 
-crumb :transactions do
-  link "商品購入"
-  parent :item_show
+crumb :item_edit do |item|
+  link "編集", edit_item_path(item)
+  parent :item_show, item
+end
+
+crumb :transactions do |item|
+  link "購入: #{item.price}円", item_transactions_path(item)
+  parent :item_show, item
 end
